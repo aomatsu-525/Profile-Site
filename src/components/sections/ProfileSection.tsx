88789d144936c7c160418a348746e3f806eebb5c
cheesestick24 <ht17a075@oecu.jp>
2025-09-26 09:49:41 +0900

    スマホ対応

--- src/components/sections/ProfileSection.tsx
+++ src/components/sections/ProfileSection.tsx
@@ -4,7 +4,7 @@ export default function ProfileSection() {
   return (
     <section className="max-w-6xl mx-auto p-4" id="profile">
       <p
-        className="text-8xl text-left ml-8 mt-16 mb-16 font-thin
+        className="text-5xl sm:text-6xl md:text-8xl text-left ml-4 sm:ml-8 mt-8 sm:mt-16 mb-8 sm:mb-16 font-thin
         bg-gradient-to-r from-pink-500 via-purple-500 to-pink-400 bg-clip-text text-transparent drop-shadow-2xl"
       >
         Profile
@@ -17,13 +17,13 @@ export default function ProfileSection() {
             alt="プロフィール画像"
             width={360}
             height={360}
-            className="rounded-full shadow-lg"
+            className="w-64 h-128 sm:w-64 sm:h-128 md:w-80 md:h-160 lg:w-96 lg:h-198 rounded-full shadow-lg object-cover"
             priority
           />
         </div>
 
-        <div className="w-full md:w-1/2 flex flex-col pt-16 pl-16 md:text-left">
-          <p className="text-xl  text-gray-600 mb-4">
+        <div className="w-full md:w-1/2 flex flex-col py-4 px-4 lg:py-16 lg:px-16 md:text-left">
+          <p className="text-base sm:text-lg md:text-xl text-gray-600 mb-2 lg:mb-4">
             うさぎが好きなおしゃべり系配信者
           </p>
           <h1 className="text-4xl md:text-6xl font-bold text-pink-600 mb-4">
