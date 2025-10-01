8db436166eff8f4cc8e199e3a5ae90a6b19577fb
haruka <ht17a075@oecu.jp>
2025-10-01 20:16:59 +0900

    ドメイン変更に伴う変更

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const repoName = "Profile-Site";
-const isGh = !!process.env.GITHUB_ACTIONS;
 
 const nextConfig: NextConfig = {
   output: 'export',
@@ -9,13 +8,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // GitHub Pages での公開時のみ basePath/assetPrefix を設定
-  ...(isGh
-    ? {
-      basePath: `/${repoName}`,
-      assetPrefix: `/${repoName}/`,
-    }
-    : {}),
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
